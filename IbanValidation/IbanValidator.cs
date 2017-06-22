e6dcc61bf74b991ae2524e5e06376043ac921fd5
Tom Hofman <paddoswam@gmail.com>
2017-06-22 16:41:28 +0200

    Feature/new iban countrycodes (#2)
    * Added new iban country codes and additional unit tests for proofing their validity.
    
    * Added Isle of Man and Jersey, also noted all CountryCodes with the country names
    
    * Added AppVeyor build badge

--- IbanValidation/IbanValidator.cs
+++ IbanValidation/IbanValidator.cs
@@ -41,70 +41,96 @@ namespace IbanValidation
 
         private static readonly Dictionary<string, int> _lengths = new Dictionary<string, int>
         {
-            {"AL", 28},
-            {"AD", 24},
-            {"AT", 20},
-            {"AZ", 28},
-            {"BE", 16},
-            {"BH", 22},
-            {"BA", 20},
-            {"BR", 29},
-            {"BG", 22},
-            {"CR", 21},
-            {"HR", 21},
-            {"CY", 28},
-            {"CZ", 24},
-            {"DK", 18},
-            {"DO", 28},
-            {"EE", 20},
-            {"FO", 18},
-            {"FI", 18},
-            {"FR", 27},
-            {"GE", 22},
-            {"DE", 22},
-            {"GI", 23},
-            {"GR", 27},
-            {"GL", 18},
-            {"GT", 28},
-            {"HU", 28},
-            {"IS", 26},
-            {"IE", 22},
-            {"IL", 23},
-            {"IT", 27},
-            {"KZ", 20},
-            {"KW", 30},
-            {"LV", 21},
-            {"LB", 28},
-            {"LI", 21},
-            {"LT", 20},
-            {"LU", 20},
-            {"MK", 19},
-            {"MT", 31},
-            {"MR", 27},
-            {"MU", 30},
-            {"MC", 27},
-            {"MD", 24},
-            {"ME", 22},
-            {"NL", 18},
-            {"NO", 15},
-            {"PK", 24},
-            {"PS", 29},
-            {"PL", 28},
-            {"PT", 25},
-            {"RO", 24},
-            {"SM", 27},
-            {"SA", 24},
-            {"RS", 22},
-            {"SK", 24},
-            {"SI", 19},
-            {"ES", 24},
-            {"SE", 24},
-            {"CH", 21},
-            {"TN", 24},
-            {"TR", 26},
-            {"AE", 23},
-            {"GB", 22},
-            {"VG", 24}
+            { "AD", 24 },	//	Andorra
+            { "AE", 23 },	//	United Arab Emirates
+            { "AL", 28 },	//	Albania
+            { "AO", 25 },	//	Angola
+            { "AT", 20 },	//	Austria
+            { "AZ", 28 },	//	Azerbaijan
+            { "BA", 20 },	//	Bosnia-Hercegovina
+            { "BE", 16 },	//	Belgium
+            { "BF", 27 },	//	Burkina Faso
+            { "BG", 22 },	//	Bulgaria
+            { "BH", 22 },	//	Bahrain
+            { "BI", 16 },	//	Burundi
+            { "BJ", 28 },	//	Benin
+            { "BR", 29 },	//	Brazil
+            { "BY", 28 },	//	Belarus
+            { "CH", 21 },	//	Switzerland
+            { "CI", 28 },	//	Ivory Coast
+            { "CM", 27 },	//	Cameroon
+            { "CR", 22 },	//	Costa Rica
+            { "CV", 25 },	//	Cape Verde
+            { "CY", 28 },	//	Cyprus
+            { "CZ", 24 },	//	Czech Republic
+            { "DE", 22 },	//	Germany
+            { "DK", 18 },	//	Denmark
+            { "DO", 28 },	//	Dominican Republic
+            { "DZ", 24 },	//	Algeria
+            { "EE", 20 },	//	Estonia
+            { "ES", 24 },	//	Spain
+            { "FI", 18 },	//	Finland
+            { "FO", 18 },	//	Faeroe Islands
+            { "FR", 27 },	//	France
+            { "GB", 22 },	//	Great Britain
+            { "GE", 22 },	//	Georgia
+            { "GI", 23 },	//	Gibraltar
+            { "GL", 18 },	//	Greenland
+            { "GR", 27 },	//	Greece
+            { "GT", 28 },	//	Guatemala
+            { "HR", 21 },	//	Croatia
+            { "HU", 28 },	//	Hungary
+            { "IE", 22 },	//	Ireland
+            { "IL", 23 },	//	Israel
+            { "IM", 22 },	//	Isle of Man
+            { "IQ", 23 },	//	Iraq
+            { "IR", 26 },	//	Iran
+            { "IS", 26 },	//	Iceland
+            { "IT", 27 },	//	Italy
+            { "JE", 22 },	//	Jersey
+            { "JO", 30 },	//	Jordan
+            { "KW", 30 },	//	Kuwait
+            { "KZ", 20 },	//	Kasakhstan
+            { "LB", 28 },	//	Lebanon
+            { "LC", 32 },	//	St Lucia
+            { "LI", 21 },	//	Liechtenstein
+            { "LT", 20 },	//	Lithuania
+            { "LU", 20 },	//	Luxembourg
+            { "LV", 21 },	//	Latvia
+            { "MC", 27 },	//	Monaco
+            { "MD", 24 },	//	Moldova
+            { "ME", 22 },	//	Montenegro
+            { "MG", 27 },	//	Madagascar
+            { "MK", 19 },	//	Macedonia
+            { "ML", 28 },	//	Mali
+            { "MR", 27 },	//	Mauritania
+            { "MT", 31 },	//	Malta
+            { "MU", 30 },	//	Mauritius
+            { "MZ", 25 },	//	Mozambique
+            { "NL", 18 },	//	Netherlands
+            { "NO", 15 },	//	Norway
+            { "PK", 24 },	//	Pakistan
+            { "PL", 28 },	//	Poland
+            { "PS", 29 },	//	Palestinian Territory
+            { "PT", 25 },	//	Portugal
+            { "QA", 29 },	//	Qatar
+            { "RO", 24 },	//	Romania
+            { "RS", 22 },	//	Serbia
+            { "SA", 24 },	//	Saudi Arabia
+            { "SC", 31 },	//	Seychelles
+            { "SE", 24 },	//	Sweden
+            { "SI", 19 },	//	Slovenia
+            { "SK", 24 },	//	Slovakia
+            { "SM", 27 },	//	San Marino
+            { "SN", 28 },	//	Senegal
+            { "ST", 25 },	//	São Tomé og Príncipe
+            { "SV", 28 },	//	El Salvador
+            { "TL", 23 },	//	Øst-Timor
+            { "TN", 24 },	//	Tunisia
+            { "TR", 26 },	//	Turkey
+            { "UA", 29 },	//	Ukraine
+            { "VG", 24 },	//	Virgin Islands
+            { "XK", 20 }	//	Kosovo
         };
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # IbanValidation
+
+[![Build status](https://ci.appveyor.com/api/projects/status/lweml30irx0ad7vs/branch/master?svg=true)](https://ci.appveyor.com/project/janv8000/ibanvalidation/branch/master)
+
 Provides IBAN bank account number validation.
 
 See [International Bank Account Number](https://en.wikipedia.org/wiki/International_Bank_Account_Number)
