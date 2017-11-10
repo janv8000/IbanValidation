ad489ab815a5d22076d2b646d2f84cb17f8eb24f
Jørn Jensen <jorn.jensen@acando.no>
2017-11-10 20:02:14 +0100

    feat: Target .NET Standard 2 for Framework and Core support
    See https://learn.microsoft.com/en-us/dotnet/standard/net-standard?tabs=net-standard-1-0#which-net-standard-version-to-target why not 1.1.
    
    * Web (MVC) integration deleted
    * Unittest all the frameworks 🤪

--- IbanValidation.Web/Scripts/jquery.validation.iban.js
+++
@@ -1,9 +0,0 @@
-﻿$.validator.addMethod("ibancheck", function (value, element) {
-    if (this.optional(element)) {
-        return "dependency-mismatch";
-    }
-
-    return IBAN.isValid(value);
-});
-
-$.validator.unobtrusive.adapters.addBool("ibancheck");
